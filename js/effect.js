7978cd4798610fc07ccbc850f4dee81a1acbc960
nguyen <tuminhnguyentmn@gmail.com>
2019-03-22 11:45:49 +0700

    commit
    create project

---
+++ js/effect.js
@@ -0,0 +1,103 @@
+﻿/* ============================== MAIN ============================== */
+function ShowLoading() {
+    $('#loading-screen').css('display', 'flex');
+
+    $('html, body').css({
+        'height': '100%',
+        'overflow': 'hidden',
+    });
+}
+
+function HideLoading() {
+    $('#loading-screen').hide();
+
+    $('html, body').css({
+        'height': 'auto',
+    });
+
+    $('html').css('overflow', 'auto');
+}
+
+function SetTabActive(index) {
+    var activeTab = $('.main-menu li').eq(index - 1);
+    activeTab.addClass('active');
+
+    // if tab need active is in submenu
+    if (activeTab.parents('li').hasClass('dropdown-li')) {
+        activeTab.parents('li').addClass('open');
+    }
+}
+
+$(function () {
+    $('#page-content').css('min-height', $(window).height() - 51);
+    
+    $(document).click(function (event) {
+        var _opened = $('#user-info-box').hasClass('open');
+        var clickover = $(event.target);
+
+        if (clickover.attr('id') == 'user-detail') return;
+        if (clickover.attr('id') == 'user-info-box') return;
+        if (clickover.closest('div').attr('id') == 'user-info-box') return;
+
+        if (_opened === true) {
+            $('#user-info-box').click();
+        }
+    });
+});
+/* ============================== MAIN ============================== */
+
+/* ============================== SIDEBAR ============================== */
+$(function () {
+    $('.dropdown-li').click(function () {
+        $(this).toggleClass('open');
+    });
+
+    // submenu link clicked
+    $('.dropdown-li .sub-menu a').click(function (e) {
+        e.stopPropagation();
+    });
+
+    $('#close-menu').click(function () {
+        $('#sidebar').removeClass('open');
+    });
+});
+/* ============================== SIDEBAR ============================== */
+
+/* ============================== NAVBAR ============================== */
+$(function () {
+    $('#user-info-box').click(function (e) {
+        $(this).toggleClass('open');
+        $('#user-detail').fadeToggle();
+    });
+
+    $('#open-menu').click(function () {
+        $('#sidebar').addClass('open');
+    });
+});
+/* ============================== NAVBAR ============================== */
+
+/* ============================== LOGIN PAGE ============================== */
+$(function () {
+    $('#login-container').css('height', $(window).height());
+});
+/* ============================== LOGIN PAGE ============================== */
+
+/* ============================== INPUT ============================== */
+$(function () {
+    $('.floating-label-container input').blur(function () {
+        if ($(this).val() != "") {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+            $(this).parents('.floating-label-container').find('label').removeClass('floated');
+        }
+    });
+
+    $('.floating-label-container label').click(function () {
+        if ($(this).hasClass('floated')) return;
+
+        $(this).addClass('floated');
+        $(this).parents('.floating-label-container').find('input').focus();
+    })
+});
+/* ============================== INPUT ============================== */
---
+++ js/handle.js
@@ -0,0 +1,69 @@
+﻿/* ============================== MAIN ============================== */
+var LOCAL_DO_URL = "/cp/do/";
+
+$(function () {
+    $('input').focus(function () {
+        $(this).removeClass('error');
+    });
+
+    $('textarea').focus(function () {
+        $(this).removeClass('error');
+    });
+
+    $('select').focus(function () {
+        $(this).removeClass('error');
+    });
+
+    // only allow number in textbox 
+    $(".accept-number").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            // Allow: Ctrl+A, Command+A
+            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Allow: home, end, left, right, down, up
+            (e.keyCode >= 35 && e.keyCode <= 40)) {
+            // let it happen, don't do anything
+            return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+});
+
+function Logout() {
+    $.post(LOCAL_DO_URL + 'admin/admin-logout.aspx', function () {
+        location.href = '/cp-login';
+    });
+}
+
+function ResetForm() {
+    $('input').each(function () {
+        $(this).val('');
+    });
+
+    $('textarea').each(function () {
+        $(this).val('');
+    });
+
+    $('select').each(function () {
+        $(this).val(0);
+    });
+}
+
+function Search(strSearch, processPath) {
+    $("#table").html('<div class="spinner spinner-2"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>');
+
+    $.post(LOCAL_DO_URL + processPath, {
+        strSearch: strSearch,
+    }, function (data) {
+        $("#table").html(data);
+    });
+}
+
+function Cancel() {
+    parent.history.back();
+    return false;
+}
+/* ============================== MAIN ============================== */
